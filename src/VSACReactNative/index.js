7bab9010db71264e30d331744fe460554247e1a9
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-19 09:54:00 -0800

    revert.

--- src/VSACReactNative/index.js
+++ src/VSACReactNative/index.js
@@ -1,7 +1,4 @@
 import { AppRegistry } from 'react-native';
-import codePush from "react-native-code-push";
 import App from './App';
 
-let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
-
-AppRegistry.registerComponent('VSACReactNative', () => codePush(codePushOptions)(App));
+AppRegistry.registerComponent('VSACReactNative', () => App);
